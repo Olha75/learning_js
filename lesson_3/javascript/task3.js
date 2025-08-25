f1f98fcbcd1489fbdf41c8c0c9e0b5d972608a29
Olha75 <137830839+Olha75@users.noreply.github.com>
2025-08-25 22:56:24 +0300

    3 task 3 finish

--- lesson_3/javascript/task3.js
+++ lesson_3/javascript/task3.js
@@ -2,5 +2,19 @@
 //Випадковим чином генерується число від 1 до 5. Спробуйте вгадати число за 2 спроби.
 //Крок 1. Введення необхідних даних
 const randomNumber = 1 + Math.floor(Math.random() * 5);
+const numberUserAttempt1 = parseInt(prompt('Пограємо? Вгадайте число від 1 до 5. У Вас 2 спроби. Погнали!', '1') || '0');
+let resultPlay;
 //Крок 2. Обчислення результатів
-//Крок 3. Виведення
+if (numberUserAttempt1 === randomNumber) {
+    resultPlay = 'Вітаю, Ви виграли!';
+}
+else {
+    const numberUserAttempt2 = parseInt(prompt('Шкода, але перша спроба невдала. Давай пробуй ще!', '1') || '0');
+    if (numberUserAttempt2 === randomNumber) {
+        resultPlay = 'Супер! Ви виграли з другої спроби! Вітаю!';
+    }
+    else {
+        resultPlay = 'Нажаль і друга спроба була невдала. Ви програли.';
+    }
+}
+document.write(resultPlay);
--- lesson_3/javascript/task7.js
+++ lesson_3/javascript/task7.js
@@ -5,23 +5,23 @@ const numberMonth = parseInt(prompt('Введіть номер місяця ві
 let season;
 //Крок 2. Обчислення результатів
 switch (numberMonth) {
-    case 1: season = 'Зима';
-    case 2: season = 'Зима';
+    case 1:
+    case 2:
     case 12:
         season = 'Зима';
         break;
-    case 3: season = 'Весна';
-    case 4: season = 'Весна';
+    case 3:
+    case 4:
     case 5:
         season = 'Весна';
         break;
-    case 6: season = 'Літо';
-    case 7: season = 'Літо';
+    case 6:
+    case 7:
     case 8:
         season = 'Літо';
         break;
-    case 9: season = 'Осінь';
-    case 10: season = 'Осінь';
+    case 9:
+    case 10:
     case 11:
         season = 'Осінь';
         break;
