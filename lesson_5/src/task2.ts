d5b50dae7a110cda2c0b7b22ea36b32f4d758897
Olha75 <137830839+Olha75@users.noreply.github.com>
2025-09-02 01:41:52 +0300

    5 task 2 zd4 st

--- lesson_5/src/task2.ts
+++ lesson_5/src/task2.ts
@@ -1,8 +1,16 @@
-     //
+     if (confirm('Почати тестування?')) {
+//Вивести таблицю з 3 рядків і 7 стовпців
 
-    //Крок 1. Введення необхідних даних
- 
+
+ document.write('<table class="tabl">')
+for (let row = 1; row <=3 ; row++) {
+     document.write('<tr>')
     
-    //Крок 2. Обчислення результатів та виведення.
-      
-    
+          for (let column = 1; column <=7; column++) {
+      document.write(`<td>${row} ${column}</td>`)
+          }
+ document.write('</tr>')
+   
+}
+ document.write('</table>')  
+}
