3fc8421f054e273b9976f3922d9c65a2b320fbbf
Olha75 <137830839+Olha75@users.noreply.github.com>
2025-09-02 06:47:09 +0300

    5 task 2 zd 4 finish

--- lesson_5/javascript/task2.js
+++ lesson_5/javascript/task2.js
@@ -1,4 +1,13 @@
 "use strict";
-//
-//Крок 1. Введення необхідних даних
-//Крок 2. Обчислення результатів та виведення.
+if (confirm('Почати тестування?')) {
+    //Вивести таблицю з 3 рядків і 7 стовпців
+    document.write('<table class="tabl">');
+    for (let row = 0; row <= 2; row++) {
+        document.write('<tr>');
+        for (let column = 1; column <= 7; column++) {
+            document.write(`<td> ${column}</td>`);
+        }
+        document.write('</tr>');
+    }
+    document.write('</table>');
+}
--- lesson_5/src/task2.ts
+++ lesson_5/src/task2.ts
@@ -1,16 +1,18 @@
      if (confirm('Почати тестування?')) {
+
 //Вивести таблицю з 3 рядків і 7 стовпців
 
+document.write('<table class="tabl">')
 
- document.write('<table class="tabl">')
-for (let row = 1; row <=3 ; row++) {
+for (let row = 0; row <=2 ; row++) {
      document.write('<tr>')
-    
+     
           for (let column = 1; column <=7; column++) {
-      document.write(`<td>${row} ${column}</td>`)
+               document.write(`<td> ${column}</td>`)
           }
- document.write('</tr>')
-   
-}
- document.write('</table>')  
+
+     document.write('</tr>')
+
+     }
+     document.write('</table>')  
 }
