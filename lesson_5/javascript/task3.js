2b8d71fbcc3dfdebcba649f562185d32552b571c
Olha75 <137830839+Olha75@users.noreply.github.com>
2025-09-02 12:20:41 +0300

    5 task 3 zd 7 correct

--- lesson_5/javascript/task3.js
+++ lesson_5/javascript/task3.js
@@ -1,16 +1,11 @@
 "use strict";
-// if (confirm('Почати тестування?')) {
-//Вивести на екран N абзаців (N вводиться користувачем) за зразком:
-// document.write('<table class="tabl">')
-let numberParagraf = parseInt(prompt('Введіть кількість абзаців', '1') || 0);
-// let numberHeading:number=parseInt()
-// let numberSection:number=parseInt()
-for (let num1 = 1; num1 <= numberParagraf; num1++) {
-    // document.write(`<h1>Заголовок ${num1}</h1><br><p>Розділ ${num1}, параграф ${num1}</p>`);
-    document.write(`<div class="books"><h1>Заголовок ${num1}</h1></div>`);
-    for (let num2 = 1; num2 <= num1; num2++) {
-        document.write(`<div class="books"><p>Розділ ${num1}, параграф ${num2}</p></div>`);
+if (confirm('Почати тестування?')) {
+    //Вивести на екран N абзаців (N вводиться користувачем) за зразком:
+    let numberParagraf = parseInt(prompt('Введіть кількість абзаців', '1') || '0');
+    for (let num1 = 1; num1 <= numberParagraf; num1++) {
+        document.write(`<div class="books"><h1>Заголовок ${num1}</h1></div>`);
+        for (let num2 = 1; num2 <= num1; num2++) {
+            document.write(`<div class="books"><p>Розділ ${num1}, параграф ${num2}</p></div>`);
+        }
     }
-    // }
 }
-// }
--- lesson_5/src/task3.ts
+++ lesson_5/src/task3.ts
@@ -1,29 +1,13 @@
-     // if (confirm('Почати тестування?')) {
+     if (confirm('Почати тестування?')) {
 
 //Вивести на екран N абзаців (N вводиться користувачем) за зразком:
 
-// document.write('<table class="tabl">')
-
-
-
-
-     
-     let numberParagraf:number=parseInt(prompt('Введіть кількість абзаців','1')||0
-     // let numberHeading:number=parseInt()
-     // let numberSection:number=parseInt()
-     
+     let numberParagraf:number=parseInt(prompt('Введіть кількість абзаців','1')||'0');  
      for (let num1 = 1; num1 <= numberParagraf; num1++) {
-          // document.write(`<h1>Заголовок ${num1}</h1><br><p>Розділ ${num1}, параграф ${num1}</p>`);
-       document.write(`<div class="books"><h1>Заголовок ${num1}</h1></div>`);
+     
+          document.write(`<div class="books"><h1>Заголовок ${num1}</h1></div>`);
           for (let num2 = 1; num2 <=num1; num2++) {
-                document.write(`<div class="books"><p>Розділ ${num1}, параграф ${num2}</p></div>`);
-         
+               document.write(`<div class="books"><p>Розділ ${num1}, параграф ${num2}</p></div>`);
      }
      
-     
-     
-     
-
-
-
-// }
+}
