297b72a12f3a3df915deeda2bcb503b01a97e046
Olha75 <137830839+Olha75@users.noreply.github.com>
2025-09-02 12:13:07 +0300

    5 task3 zd 7 finish

--- lesson_5/src/task3.ts
+++ lesson_5/src/task3.ts
@@ -1,6 +1,29 @@
-     //
+     // if (confirm('Почати тестування?')) {
 
-    //Крок 1. Введення необхідних даних
- 
+//Вивести на екран N абзаців (N вводиться користувачем) за зразком:
 
-    //Крок 2. Обчислення результатів
+// document.write('<table class="tabl">')
+
+
+
+
+     
+     let numberParagraf:number=parseInt(prompt('Введіть кількість абзаців','1')||0
+     // let numberHeading:number=parseInt()
+     // let numberSection:number=parseInt()
+     
+     for (let num1 = 1; num1 <= numberParagraf; num1++) {
+          // document.write(`<h1>Заголовок ${num1}</h1><br><p>Розділ ${num1}, параграф ${num1}</p>`);
+       document.write(`<div class="books"><h1>Заголовок ${num1}</h1></div>`);
+          for (let num2 = 1; num2 <=num1; num2++) {
+                document.write(`<div class="books"><p>Розділ ${num1}, параграф ${num2}</p></div>`);
+         
+     }
+     
+     
+     
+     
+
+
+
+// }
