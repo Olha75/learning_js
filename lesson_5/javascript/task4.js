e43390c9f807f8203504a5cb0caa6d3dcddc2627
Olha75 <137830839+Olha75@users.noreply.github.com>
2025-09-02 18:50:26 +0300

    5 task 4 dz 12 finish

--- lesson_5/javascript/task4.js
+++ lesson_5/javascript/task4.js
@@ -7,21 +7,47 @@ if (confirm('Почати тестування?')) {
     //  три третіх – 500,
     //  три четвертих зображення – 1000грн).
     //  Використати цикли і switch (для вибору зображення за номером).
-    const randImgNum = 1 + Math.floor(Math.random() * 4);
-    let imgSrc;
-    switch (randImgNum) {
-        case 1:
-            imgSrc = '../images/lemon.jpg';
-            break;
-        case 2:
-            imgSrc = '../images/orange.jpg';
-            break;
-        case 3:
-            imgSrc = '../images/grapes.jpg';
-            break;
-        case 4:
-            imgSrc = '../images/watermelon.jpg';
-            break;
+    let totalPrice = 0;
+    let img1, img2, img3;
+    for (let num = 1; num <= 3; num++) {
+        const randImgNum = 1 + Math.floor(Math.random() * 4);
+        let imgSrc;
+        let imgName;
+        let priseWin = 0;
+        switch (randImgNum) {
+            case 1:
+                imgSrc = '../images/lemon.jpg';
+                imgName = 'lemon';
+                priseWin = 100;
+                break;
+            case 2:
+                imgSrc = '../images/orange.jpg';
+                imgName = 'orange';
+                priseWin = 200;
+                break;
+            case 3:
+                imgSrc = '../images/grapes.jpg';
+                imgName = 'grapes';
+                priseWin = 500;
+                break;
+            case 4:
+                imgSrc = '../images/watermelon.jpg';
+                imgName = 'watermelon';
+                priseWin = 1000;
+                break;
+        }
+        document.write(`<img src=${imgSrc}>`);
+        totalPrice += priseWin;
+        if (num === 1)
+            img1 = imgName;
+        if (num === 2)
+            img2 = imgName;
+        if (num === 3)
+            img3 = imgName;
     }
-    document.write(`<img src=${imgSrc}><img src=${imgSrc}><img src=${imgSrc}>`);
+    if (img1 === img2 && img2 === img3) {
+        document.write(`<h2 class="win">Вітаємо! Ви виграли ${totalPrice} грн</h2>`);
+    }
+    else
+        document.write('<h2 class="loser">Вибачте, але сьогодні виграло КАЗИНО. Не грайте більше!</h2>');
 }
