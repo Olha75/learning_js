744c23dc6ffca4cb3c06ad18d657b9d041112c8b
Olha75 <137830839+Olha75@users.noreply.github.com>
2025-09-03 22:27:56 +0300

    6 templ

--- lesson_6/src/task1.ts
+++ lesson_6/src/task1.ts
@@ -1,17 +1,5 @@
      if (confirm('Почати тестування?')) {
-//Створити 10 елементів для введення цін продуктів
 
-let priseProduct:number=parseFloat('0')
-for (let numberProduct = 1; numberProduct <= 10; numberProduct++) {
-     document.write(`<div class="body_product">
-                    <div class='num_product'><h3>Product #${numberProduct}</h3></div>
-                    <input  
-                    class="product_list" 
-                    required 
-                    name="product-list" 
-                    type='number' 
-                    placeholder="Введіть ціну продукту цифрами" ${priseProduct.toFixed(2)}></div>`)
-     }
 }
 
 
