6ab8586c989dd69da384d9fbf450c6923d4ad3c1
Olha75 <137830839+Olha75@users.noreply.github.com>
2025-09-10 21:19:37 +0300

    6 task 1 korrect

--- lesson_6/src/task1.ts
+++ lesson_6/src/task1.ts
@@ -1,19 +1,19 @@
 if (confirm('Почати тестування?')) {
-  //2.1. Відобразити трикутник за допомогою символів «о»
+	//2.1. Відобразити трикутник за допомогою символів «о»
 
-  for (let rowNumber = 1; rowNumber <= 7; rowNumber++) {
-    let rowPyramid = '';
-    for (let spaces = 0; spaces < 7 - rowNumber; spaces++) {
-      rowPyramid += '\u2009 ';
-    }
+	for (let rowNumber = 1; rowNumber <= 7; rowNumber++) {
+		let rowPyramid = '';
+		for (let spaces = 0; spaces < 7 - rowNumber; spaces++) {
+			rowPyramid += '\u2009 ';
+		}
 
-    for (let symbol = 0; symbol < rowNumber; symbol++) {
-      rowPyramid += 'o';
-      if (symbol < rowNumber - 1) {
-        rowPyramid += '\u2009 ';
-      }
-    }
+		for (let symbol = 0; symbol < rowNumber; symbol++) {
+			rowPyramid += 'o';
+			if (symbol < rowNumber - 1) {
+				rowPyramid += '\u2009 ';
+			}
+		}
 
-    document.write(rowPyramid + '<br>');
-  }
+		document.write(rowPyramid + '<br>');
+	}
 }
