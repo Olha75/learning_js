cb61e48b494646c529e3c164d998d34d72b9c4e4
Olha75 <137830839+Olha75@users.noreply.github.com>
2025-09-10 21:48:08 +0300

    6 task 1 zd 2.1 finish

--- lesson_6/javascript/task1.js
+++ lesson_6/javascript/task1.js
@@ -1,15 +1,16 @@
 "use strict";
 if (confirm('Почати тестування?')) {
     //2.1. Відобразити трикутник за допомогою символів «о»
+    const thinSpace = '\u2009';
     for (let rowNumber = 1; rowNumber <= 7; rowNumber++) {
         let rowPyramid = '';
         for (let spaces = 0; spaces < 7 - rowNumber; spaces++) {
-            rowPyramid += '\u2009 ';
+            rowPyramid += `${thinSpace}`;
         }
         for (let symbol = 0; symbol < rowNumber; symbol++) {
             rowPyramid += 'o';
             if (symbol < rowNumber - 1) {
-                rowPyramid += '\u2009 ';
+                rowPyramid += '';
             }
         }
         document.write(rowPyramid + '<br>');
